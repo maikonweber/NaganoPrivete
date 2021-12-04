9e3af962b27a6908e64dcdcd1e70b8d247aa7d70
unknown <maikonweber@gmail.com>
2021-12-04 20:18:16 -0300

    eslint

--- pages/api/createLead.js
+++ pages/api/createLead.js
@@ -1,8 +1,8 @@
-import { insertLeads } from '../db/database'        
+// import { insertLeads } from '../src/db/database'        
 
-export default async function createLeads(req, res) {
-    if (req.method === 'POST') {
-        const retorno  =  await insertLeads(req.body.nome, req.body.sobrenome, req.body.telefone, req.body.link, req.body.valor, req.body.produto, req.body.email);
-        res.json(retorno);
-    }
-}
+// export default async function createLeads(req, res) {
+//     if (req.method === 'POST') {
+//         const retorno  =  await insertLeads(req.body.nome, req.body.sobrenome, req.body.telefone, req.body.link, req.body.valor, req.body.produto, req.body.email);
+//         res.json(retorno);
+//     }
+// }
