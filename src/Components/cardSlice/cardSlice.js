e09a4222e83ae7f4f7012a28feb3b73a2676a1eb
Gabriel Natan <gabriel--natan@live.com>
2021-12-19 13:04:56 -0300

    Ajustes consignados

--- src/Components/cardSlice/cardSlice.js
+++ src/Components/cardSlice/cardSlice.js
@@ -3,6 +3,7 @@ import Style from "./cardSlice.module.css"
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import React from 'react';
+import { Button } from "../Topbar/styled";
 
 
 
@@ -17,41 +18,41 @@ function CardSlice() {
         // Check if document is finally loaded
     
 
-        }
+    }
 
+        // React.useEffect(() => {
+        //     const slider = document.getElementsByClassName('MuiSlider-thumbColorPrimary MuiSlider-thumbSizeMedium MuiSlider-thumb css-eg0mwd-MuiSlider-thumb'); 
+        //     slider[0].addEventListener('click', (event) => {
+        //        const value = event.target.style.left;
+        //        const value2 = value.slice(0, -1);
+        //        const result = 2000 * parseInt(value);
+        //        setValue(result);
 
-        React.useEffect(() => {
-            const slider = document.getElementsByClassName('MuiSlider-thumbColorPrimary MuiSlider-thumbSizeMedium MuiSlider-thumb css-eg0mwd-MuiSlider-thumb'); 
-            slider[0].addEventListener('click', (event) => {
-               const value = event.target.style.left;
-               const value2 = value.slice(0, -1);
-               const result = 2000 * parseInt(value);
-               setValue(result);
-               
-            
-                
-
-            });
-            
+        //     });
              
-        }, []);
+        // }, []);
 
    
+        const valueSelect = (e)=>{
+            const result = 2000 * parseInt(e.target.value);
+            setValue(result);
 
+        }
 
     return (
+        <>
         <div className={Style.container}>
-            <div className={Style.intro}></div>
             <div className={Style.text}>
                 <h1 className={Style.textTitle}>Qual o Valor?</h1>
-                <h2 className={Style.textTitle} > {value} </h2>
-                <Box sx={{ width: 300 }}>
-                <Slider id='slider' defaultValue={50}  />
+                <h2 className={Style.textValue} > {value} </h2>
+                <Box sx={{ width: "100%",padding:"15px 0 30px 0" }}>
+                    <Slider onChange={(e)=>{valueSelect(e)}} id='slider' defaultValue={50}  />
                 </Box>
-            </div>              
+                <button>Simule</button>
+            </div>
         </div >
-    
-         
+
+         </>
             
     )
 }
