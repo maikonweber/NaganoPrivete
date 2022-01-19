15b3efd8b3e256829c76007c2b0fbca05970e75b
Maikon <m.carvalho@grouplinknetwork.com>
2022-01-19 12:27:57 -0300

    add all

--- src/services/services.js
+++ src/services/services.js
@@ -12,7 +12,7 @@ export async function v1_teste(token) {
   }
   
   export async function loginIn(email, password) {
-    const response = await fetch(`https://${process.env.NEXT_PUBLIC_API_URL}login`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -25,7 +25,7 @@ export async function v1_teste(token) {
   }
   
   export async function getAllLeads(token) {
-    const response = await fetch(`https://${process.env.NEXT_PUBLIC_API_URL}data/getAll`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}data/getAll`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json',
       'token': `${token}` },
@@ -37,7 +37,7 @@ export async function v1_teste(token) {
   
   export async function insertLead(data) {
     console.log(data)
-    const response = await fetch(`https://${process.env.NEXT_PUBLIC_API_URL}/sendLead`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}sendLead`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body : JSON.stringify({
